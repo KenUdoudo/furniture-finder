23ba547ee5ecec4fc83622e93502da1b9c7e2708
Ken Udoudo <kendo302003@gmail.com>
2024-06-22 02:14:18 -0700

    looks better

--- src/pages/L1.jsx
+++ src/pages/L1.jsx
@@ -23,6 +23,13 @@ const L1 = () => {
         <p className="text-[20px] lg:text-[25px] font-medium">Furnitures</p>
         <MdOutlineKeyboardArrowRight className="md:w-[20px] md:h-[20px] lg:w-[30px] lg:h-[30px]" />
       </div>
+
+      <div className="flex flex-wrap justify-center items-center mt-[20px] gap-[10px]">
+        <div className="border-[2px] border-black rounded-md w-[150px]">
+            <div className="w-full h-[150px] bg-[#312b2b]"></div>
+            <p className="font-bold text-[13px] text-center py-[10px]">Chaise Lounge Chairs</p>
+        </div>
+      </div>
     </div>
   );
 };
