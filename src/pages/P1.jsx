dadf4363d8bd3edc6f58f2dc0baf74e97c053733
Ken Udoudo <kendo302003@gmail.com>
2024-06-23 07:47:41 -0700

    done with p1

--- src/pages/P1.jsx
+++ src/pages/P1.jsx
@@ -4,6 +4,7 @@ import P3 from "../Pictures/P1/3.png";
 import P4 from "../Pictures/P1/4.png";
 import one from "../Pictures/P1furniture/one.png";
 import two from "../Pictures/P1furniture/two.png";
+import three from "../Pictures/P1furniture/three.png";
 const L1 = () => {
   return (
     <div className="px-[20px] lg:px-[50px]">
@@ -26,7 +27,7 @@ const L1 = () => {
         <MdOutlineKeyboardArrowRight className="md:w-[20px] md:h-[20px] lg:w-[30px] lg:h-[30px]" />
       </div>
 
-      <div className="flex flex-wrap justify-center items-center mt-[20px] gap-[10px]">
+      <div className="flex flex-wrap justify-center mt-[20px] gap-[10px]">
         <div className="border-[2px] border-black rounded-md w-[150px] md:w-[250px]">
             <img src={ one } className="object-cover w-full h-[150px] md:h-[250px]"/>
             <p className="font-bold text-[13px] md:text-[16px] text-center py-[10px]">Kalapalo Headdress Painting</p>
@@ -34,6 +35,10 @@ const L1 = () => {
         <div className="border-[2px] border-black rounded-md w-[150px] md:w-[250px]">
             <img src={ two } className="object-cover w-full h-[150px] md:h-[250px]"/>
             <p className="font-bold text-[13px] md:text-[16px] text-center py-[10px]">Lamp Model Nanas</p>
+        </div>
+        <div className="border-[2px] border-black rounded-md w-[150px] md:w-[250px]">
+            <img src={ three } className="object-cover w-full h-[150px] md:h-[250px]"/>
+            <p className="font-bold text-[13px] md:text-[16px] text-center py-[10px]">Kilim Rug</p>
         </div>
       </div>
     </div>
