a9853ab3ab6c535aa19c4a895b236357305d609a
Ken Udoudo <kendo302003@gmail.com>
2024-06-23 08:08:51 -0700

    done

--- src/pages/L1.jsx
+++ src/pages/L1.jsx
@@ -2,6 +2,13 @@ import { MdOutlineKeyboardArrowRight } from "react-icons/md";
 import L2 from "../Pictures/L1/2.png";
 import L3 from "../Pictures/L1/3.png";
 import L4 from "../Pictures/L1/4.png";
+import one from "../Pictures/L1furniture/one.png";
+import two from "../Pictures/L1furniture/two.png";
+import three from "../Pictures/L1furniture/three.png";
+import four from "../Pictures/L1furniture/four.png";
+import five from "../Pictures/L1furniture/five.png";
+import six from "../Pictures/L1furniture/six.png";
+import seven from "../Pictures/L1furniture/seven.png";
 const L1 = () => {
   return (
     <div className="px-[20px] lg:px-[50px]">
@@ -24,10 +31,34 @@ const L1 = () => {
         <MdOutlineKeyboardArrowRight className="md:w-[20px] md:h-[20px] lg:w-[30px] lg:h-[30px]" />
       </div>
 
-      <div className="flex flex-wrap justify-center items-center mt-[20px] gap-[10px]">
+      <div className="flex flex-wrap justify-center mt-[20px] gap-[10px]">
         <div className="border-[2px] border-black rounded-md w-[150px] md:w-[250px]">
-            <div className="w-full h-[150px] md:h-[250px] bg-[#312b2b]"></div>
-            <p className="font-bold text-[13px] md:text-[16px] text-center py-[10px]">Chaise Lounge Chairs</p>
+            <img src={ one } className="object-cover w-full h-[150px] md:h-[250px]"/>
+            <p className="font-bold text-[13px] md:text-[16px] text-center py-[10px]">Racer Dining Chair</p>
+        </div>
+        <div className="border-[2px] border-black rounded-md w-[150px] md:w-[250px]">
+            <img src={ two } className="object-cover w-full h-[150px] md:h-[250px]"/>
+            <p className="font-bold text-[13px] md:text-[16px] text-center py-[10px]">Tulip Mid Century Dining Table</p>
+        </div>
+        <div className="border-[2px] border-black rounded-md w-[150px] md:w-[250px]">
+            <img src={ three } className="object-cover w-full h-[150px] md:h-[250px]"/>
+            <p className="font-bold text-[13px] md:text-[16px] text-center py-[10px]">Seating Islands</p>
+        </div>
+        <div className="border-[2px] border-black rounded-md w-[150px] md:w-[250px]">
+            <img src={ four } className="object-cover w-full h-[150px] md:h-[250px]"/>
+            <p className="font-bold text-[13px] md:text-[16px] text-center py-[10px]">HAY PAO Portable Lamp</p>
+        </div>
+        <div className="border-[2px] border-black rounded-md w-[150px] md:w-[250px]">
+            <img src={ five } className="object-cover w-full h-[150px] md:h-[250px]"/>
+            <p className="font-bold text-[13px] md:text-[16px] text-center py-[10px]">Sculptural Glassed Candlesticks</p>
+        </div>
+        <div className="border-[2px] border-black rounded-md w-[150px] md:w-[250px]">
+            <img src={ six } className="object-cover w-full h-[150px] md:h-[250px]"/>
+            <p className="font-bold text-[13px] md:text-[16px] text-center py-[10px]">Blu Dot Lap 4 Drawer Dresser</p>
+        </div>
+        <div className="border-[2px] border-black rounded-md w-[150px] md:w-[250px]">
+            <img src={ seven } className="object-cover w-full h-[150px] md:h-[250px]"/>
+            <p className="font-bold text-[13px] md:text-[16px] text-center py-[10px]">Normann Copenhagen Turn Side Table</p>
         </div>
       </div>
     </div>
